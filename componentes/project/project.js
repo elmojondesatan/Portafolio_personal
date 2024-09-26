dbba550f310e59a00223976725ce73fcc8e3132e
pijije <davillatoro@scl.edu.gt>
2024-09-26 10:05:44 -0600

    Segunda version del portafolio -Se agrega el nvBar -Se agrega el header -se agrega un elemento en guia

--- componentes/project/project.js
+++ componentes/project/project.js
@@ -1,3 +1,4 @@
+import { header } from "../header/header.js";
 function proyectos(){
     let section = document.createElement('section');
     section.appendChild(header());
@@ -7,21 +8,6 @@ function proyectos(){
 
 }
 
-function header(){
-    let div = document.createElement('div');
-    div.className = 'header';
-
-    let h2 = document.createElement('h2');
-    h2.innerHTML = "Proyectos";
-    div.appendChild(h2);
-
-    let btn = document.createElement('div');
-    btn.innerHTML = 'GitHub';
-    div.appendChild(btn);
-    
-    return div;
-
-}
 
 function item(){
     let div = document.createElement('item');
